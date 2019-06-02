29792a3cc09dd4b138025cbd7bead9c817052f33
Grumstukas <tartenaite.toma@gmail.com>
2019-06-02 21:19:39 +0300

    pataysyta x formule

---
+++ js/actions.js
@@ -0,0 +1,46 @@
+function myFunction() {
+    var columns, rows, index;
+  
+  var rows = document.getElementById("rows").value;
+  var columns = document.getElementById("columns").value;
+  var index = document.getElementById("index").value;
+  // var rows = 10;
+  // var columns = 8;
+  // var index = 81;
+  if((document.getElementById('columns').value === '') && (document.getElementById('rows').value === ''))
+  {document.getElementById("demo").innerHTML =
+"Būk geras, įrašyk ką nors, nes be duomenų nieko suskaičiuoti negaliu. Ačiū :)"}
+
+    else if ((columns < 1) && (rows < 1 )) {
+        document.getElementById("demo").innerHTML =
+    "Stulpeliai ir eilutės šachmatų lentoje skaičiuojami teigiamais skaičiais :), skaičiai kurį įvedei - neteisingi"
+    } 
+    else if (columns < 1) {
+        document.getElementById("demo").innerHTML =
+    "Stulpeliai šachmatų lentoje skaičiuojami teigiamais skaičiais :), skaičius kurį įvedei - neteisingas"
+    } 
+    else if (rows < 1 ){
+        document.getElementById("demo").innerHTML =
+    "Eilutės šachmatų lentoje skaičiuojami teigiamais skaičiais :), skaičius kurį įvedei - neteisingas"
+    } 
+    else if (index < 0 ){
+        document.getElementById("demo").innerHTML =
+    "Tokio langelio lentoje tikrai nėra :)"}
+    else if (document.getElementById('index').value === '')
+    {document.getElementById("demo").innerHTML =
+    "Tai kokio langelio koordinates norėtum sužinoti ???"}
+else {
+  if ( (index % columns) > 0 ){
+      var x = ( (index % columns) - 1 );}
+      else {
+      var x = (columns - 1);}
+  
+  var y = Math.floor((index - x) / columns);
+  
+  if( index > (rows * columns)){
+      document.getElementById("demo").innerHTML =
+  "Langelio koordinačių nustatyti negalima, nes ieškomo langelio lentoje tiesiog nėra. Bandyk dar kartą ;)" 
+  } else {
+  document.getElementById("demo").innerHTML =
+  "Langelio " + index + " kordinatės yra " + ': ( '+x+'; '+y+' )' ; }}}
+  
